3d80dd341d05a9ad075666a0e3a9d49228fe9013
Jared Erickson <jared.erickson@gmail.com>
2013-05-19 13:53:59 -0700

    Remove demo commands.

---
+++ src/main/java/org/geoserver/shell/FeatureTypeCommands.java
@@ -0,0 +1,11 @@
+package org.geoserver.shell;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jericks
+ * Date: 5/12/13
+ * Time: 5:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FeatureTypeCommands {
+}
