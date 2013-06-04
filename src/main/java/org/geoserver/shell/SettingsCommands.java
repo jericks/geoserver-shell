3a215b5fa0de6eafaaa9173d1f4d3ab2524d8f15
Jared Erickson <jared.erickson@gmail.com>
2013-06-03 19:27:36 -0700

    Add settings modify, settings contact list, and settings contact modify commands

--- src/main/java/org/geoserver/shell/SettingsCommands.java
+++ src/main/java/org/geoserver/shell/SettingsCommands.java
@@ -3,11 +3,17 @@ package org.geoserver.shell;
 import it.geosolutions.geoserver.rest.HTTPUtils;
 import it.geosolutions.geoserver.rest.decoder.utils.JDOMBuilder;
 import org.jdom.Element;
+import org.jdom.input.SAXBuilder;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
+import org.springframework.shell.core.annotation.CliOption;
 import org.springframework.shell.support.util.OsUtils;
 import org.springframework.stereotype.Component;
+
+import java.io.StringReader;
 
 @Component
 public class SettingsCommands implements CommandMarker {
@@ -71,6 +77,218 @@ public class SettingsCommands implements CommandMarker {
         builder.append(TAB).append("Global Services: ").append(root.getChildText("globalServices")).append(OsUtils.LINE_SEPARATOR);
         builder.append(TAB).append("XML Post Request Log Buffer Size: ").append(root.getChildText("xmlPostRequestLogBufferSize")).append(OsUtils.LINE_SEPARATOR);
         return builder.toString();
+    }
+
+    @CliCommand(value = "settings modify", help = "Modify settings.")
+    public boolean modify(
+            // Settings
+            @CliOption(key = "charset", mandatory = false, help = "The charset") String charset,
+            @CliOption(key = "numdecimals", mandatory = false, help = "The number of decimals") String numberOfDecimals,
+            @CliOption(key = "onlineresource", mandatory = false, help = "The oneline resource url") String onlineResource,
+            @CliOption(key = "verbose", mandatory = false, help = "The verbose flag (true | false)") String verbose,
+            @CliOption(key = "verboseexceptions", mandatory = false, help = "The verbose exceptions flag (true | false)") String verboseExceptions,
+            // Global
+            @CliOption(key = "updatesequence", mandatory = false, help = "The update sequence") String updateSequence,
+            @CliOption(key = "featuretyepcachesize", mandatory = false, help = "The feature type cache size") String featureTypeCacheSize,
+            @CliOption(key = "globalservices", mandatory = false, help = "The global service flag (true | false)") String globalServices,
+            @CliOption(key = "xmlpostrequestlogbuffersize", mandatory = false, help = "The xml post request log buffer size") String xmlPostRequestLogBufferSize,
+            // JAI
+            @CliOption(key = "allowinterpolation", mandatory = false, help = "The JAI flag to allow interpolation or not") String allowInterpolation,
+            @CliOption(key = "recycling", mandatory = false, help = "The JAI flag to allow recycling") String recycling,
+            @CliOption(key = "tilepriority", mandatory = false, help = "The JAI tile priority") String tilePriority,
+            @CliOption(key = "tilethreads", mandatory = false, help = "The JAI tile threads") String tileThreads,
+            @CliOption(key = "memorycapacity", mandatory = false, help = "The JAI memory capacity") String memoryCapacity,
+            @CliOption(key = "memorythreshold", mandatory = false, help = "The JAI memory threshold") String memoryThreshold,
+            @CliOption(key = "imageiocache", mandatory = false, help = "The JAI flag to allow image IO cache") String imageIOCache,
+            @CliOption(key = "pngacceleration", mandatory = false, help = "The JAI flag to allow PNG acceleration") String pngAcceleration,
+            @CliOption(key = "jpegacceleration", mandatory = false, help = "The JAI flag to allow JPEG acceleration") String jpegAcceleration,
+            @CliOption(key = "allownativemosaic", mandatory = false, help = "The JAI flag to allow native mosaic") String allowNativeMosaic,
+            // Coverage Access
+            @CliOption(key = "maxpoolsize", mandatory = false, help = "The coverage access parameter to set the max pool size") String maxPoolSize,
+            @CliOption(key = "corepoolsize", mandatory = false, help = "The coverage access parameter to set the core pool size") String corePoolSize,
+            @CliOption(key = "keepalivetime", mandatory = false, help = "The coverage access parameter to set the keep alive time") String keepAliveTime,
+            @CliOption(key = "queuetype", mandatory = false, help = "The coverage access parameter to set the queue type") String queueType,
+            @CliOption(key = "imageiocachethreshold", mandatory = false, help = "The coverage access parameter to set the image io cache threshold") String imageIOCacheThreshold,
+            // Contact
+            @CliOption(key = "person", mandatory = false, help = "The contact's name") String person,
+            @CliOption(key = "position", mandatory = false, help = "The contact's position") String position,
+            @CliOption(key = "email", mandatory = false, help = "The contact's email") String email,
+            @CliOption(key = "organization", mandatory = false, help = "The contact's organization") String organization,
+            @CliOption(key = "city", mandatory = false, help = "The contact's city") String city,
+            @CliOption(key = "country", mandatory = false, help = "The contact's country") String country,
+            @CliOption(key = "addresstype", mandatory = false, help = "The contact's address type") String addressType
+    ) throws Exception {
+        String url = geoserver.getUrl() + "/rest/settings.xml";
+        String xml = HTTPUtils.get(url, geoserver.getUser(), geoserver.getPassword());
+        Element element = JDOMBuilder.buildElement(xml);
+        // Settings
+        Element settingsElement = element.getChild("settings");
+        if (charset != null) {
+            getOrAdd(settingsElement, "charset").setText(charset);
+        }
+        if (numberOfDecimals != null) {
+            getOrAdd(settingsElement, "numDecimals").setText(numberOfDecimals);
+        }
+        if (onlineResource != null) {
+            getOrAdd(settingsElement, "onlineResource").setText(onlineResource);
+        }
+        if (verbose != null) {
+            getOrAdd(settingsElement, "verbose").setText(verbose);
+        }
+        if (verboseExceptions != null) {
+            getOrAdd(settingsElement, "verboseExceptions").setText(verboseExceptions);
+        }
+        // Contacts
+        Element contactElement = settingsElement.getChild("contact");
+        if (person != null) {
+            getOrAdd(contactElement,"contactPerson").setText(person);
+        }
+        if (position != null) {
+            getOrAdd(contactElement,"contactPosition").setText(position);
+        }
+        if (email != null) {
+            getOrAdd(contactElement,"contactEmail").setText(email);
+        }
+        if (organization != null) {
+            getOrAdd(contactElement,"contactOrganization").setText(organization);
+        }
+        if (city != null) {
+            getOrAdd(contactElement,"addressCity").setText(city);
+        }
+        if (country != null) {
+            getOrAdd(contactElement,"addressCountry").setText(country);
+        }
+        if (addressType != null) {
+            getOrAdd(contactElement,"addressType").setText(addressType);
+        }
+        // JAI
+        Element jaiElement = element.getChild("jai");
+        if (allowInterpolation != null) {
+            getOrAdd(jaiElement,"allowInterpolation").setText(allowInterpolation);
+        }
+        if (recycling != null) {
+            getOrAdd(jaiElement,"recycling").setText(recycling);
+        }
+        if (tilePriority != null) {
+            getOrAdd(jaiElement,"tilePriority").setText(tilePriority);
+        }
+        if (tileThreads != null) {
+            getOrAdd(jaiElement,"tileThreads").setText(tileThreads);
+        }
+        if (memoryCapacity != null) {
+            getOrAdd(jaiElement,"memoryCapacity").setText(memoryCapacity);
+        }
+        if (memoryThreshold != null) {
+            getOrAdd(jaiElement,"memoryThreshold").setText(memoryThreshold);
+        }
+        if (imageIOCache != null) {
+            getOrAdd(jaiElement,"imageIOCache").setText(imageIOCache);
+        }
+        if (pngAcceleration != null) {
+            getOrAdd(jaiElement,"pngAcceleration").setText(pngAcceleration);
+        }
+        if (jpegAcceleration != null) {
+            getOrAdd(jaiElement,"jpegAcceleration").setText(jpegAcceleration);
+        }
+        if (allowNativeMosaic != null) {
+            getOrAdd(jaiElement,"allowNativeMosaic").setText(allowNativeMosaic);
+        }
+        // Coverage Access
+        Element coverageAccessElement = element.getChild("coverageAccess");
+        if (maxPoolSize != null) {
+            getOrAdd(coverageAccessElement,"maxPoolSize").setText(maxPoolSize);
+        }
+        if (corePoolSize != null) {
+            getOrAdd(coverageAccessElement,"corePoolSize").setText(corePoolSize);
+        }
+        if (keepAliveTime != null) {
+            getOrAdd(coverageAccessElement,"keepAliveTime").setText(keepAliveTime);
+        }
+        if (queueType != null) {
+            getOrAdd(coverageAccessElement,"queueType").setText(queueType);
+        }
+        if (imageIOCacheThreshold != null) {
+            getOrAdd(coverageAccessElement,"imageIOCacheThreshold").setText(imageIOCacheThreshold);
+        }
+        // Global
+        if (updateSequence != null) {
+            getOrAdd(element,"updateSequence").setText(updateSequence);
+        }
+        if (featureTypeCacheSize != null) {
+            getOrAdd(element,"featureTypeCacheSize").setText(featureTypeCacheSize);
+        }
+        if (globalServices != null) {
+            getOrAdd(element,"globalServices").setText(globalServices);
+        }
+        if (xmlPostRequestLogBufferSize != null) {
+            getOrAdd(element,"xmlPostRequestLogBufferSize").setText(xmlPostRequestLogBufferSize);
+        }
+        String content = new XMLOutputter(Format.getPrettyFormat()).outputString(element);
+        String response = HTTPUtils.putXml(url, content, geoserver.getUser(), geoserver.getPassword());
+        return response != null;
+    }
+
+    private Element getOrAdd(Element element, String childName) {
+        Element child = element.getChild(childName);
+        if (child == null) {
+            child = new Element(childName);
+            element.addContent(child);
+        }
+        return child;
+    }
+
+    @CliCommand(value = "settings contact list", help = "List contact settings.")
+    public String listContact() throws Exception {
+        String TAB = "   ";
+        String xml = HTTPUtils.get(geoserver.getUrl() + "/rest/settings/contact.xml", geoserver.getUser(), geoserver.getPassword());
+        StringBuilder builder = new StringBuilder();
+        Element element = JDOMBuilder.buildElement(xml);
+        builder.append("City: ").append(element.getChildText("addressCity")).append(OsUtils.LINE_SEPARATOR);
+        builder.append("Country: ").append(element.getChildText("addressCountry")).append(OsUtils.LINE_SEPARATOR);
+        builder.append("Type: ").append(element.getChildText("addressType")).append(OsUtils.LINE_SEPARATOR);
+        builder.append("Email: ").append(element.getChildText("contactEmail")).append(OsUtils.LINE_SEPARATOR);
+        builder.append("Organization: ").append(element.getChildText("contactOrganization")).append(OsUtils.LINE_SEPARATOR);
+        builder.append("Name: ").append(element.getChildText("contactPerson")).append(OsUtils.LINE_SEPARATOR);
+        builder.append("Position: ").append(element.getChildText("contactPosition")).append(OsUtils.LINE_SEPARATOR);
+        return builder.toString();
+    }
+
+    @CliCommand(value = "settings contact modify", help = "Modify contact settings.")
+    public boolean modifyContact(
+            @CliOption(key = "person", mandatory = false, help = "The contact's name") String person,
+            @CliOption(key = "position", mandatory = false, help = "The contact's position") String position,
+            @CliOption(key = "email", mandatory = false, help = "The contact's email") String email,
+            @CliOption(key = "organization", mandatory = false, help = "The contact's organization") String organization,
+            @CliOption(key = "city", mandatory = false, help = "The contact's city") String city,
+            @CliOption(key = "country", mandatory = false, help = "The contact's country") String country,
+            @CliOption(key = "addresstype", mandatory = false, help = "The contact's address type") String addressType
+    ) throws Exception {
+        String url = geoserver.getUrl() + "/rest/settings/contact.xml";
+        Element element = new Element("contact");
+        if (person != null) {
+            element.addContent(new Element("contactPerson").setText(person));
+        }
+        if (position != null) {
+            element.addContent(new Element("contactPosition").setText(position));
+        }
+        if (email != null) {
+            element.addContent(new Element("contactEmail").setText(email));
+        }
+        if (organization != null) {
+            element.addContent(new Element("contactOrganization").setText(organization));
+        }
+        if (city != null) {
+            element.addContent(new Element("addressCity").setText(city));
+        }
+        if (country != null) {
+            element.addContent(new Element("addressCountry").setText(country));
+        }
+        if (addressType != null) {
+            element.addContent(new Element("addressType").setText(addressType));
+        }
+        String content = new XMLOutputter(Format.getPrettyFormat()).outputString(element);
+        String response = HTTPUtils.putXml(url, content, geoserver.getUser(), geoserver.getPassword());
+        return response != null;
     }
 
 }
