3cadf03b48ac7c264856267adafe5e7f654791bd
Jared Erickson <jared.erickson@gmail.com>
2013-06-04 20:02:23 -0700

    Add JDOM utility class

---
+++ src/main/java/org/geoserver/shell/JDOMUtil.java
@@ -0,0 +1,22 @@
+package org.geoserver.shell;
+
+import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
+
+public class JDOMUtil {
+
+    public static Element getOrAdd(Element element, String childName) {
+        Element child = element.getChild(childName);
+        if (child == null) {
+            child = new Element(childName);
+            element.addContent(child);
+        }
+        return child;
+    }
+
+    public static String toString(Element element) {
+        XMLOutputter xmlOutputter = new XMLOutputter(Format.getPrettyFormat());
+        return xmlOutputter.outputString(element);
+    }
+}
