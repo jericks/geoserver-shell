11b709f822aa409d5416d583562fcad373ad6cd8
Jared Erickson <jared.erickson@gmail.com>
2013-06-10 20:00:28 -0700

    Add workspace tests for getting and setting the default

--- src/test/java/org/geoserver/shell/WorkspaceCommandsTest.java
+++ src/test/java/org/geoserver/shell/WorkspaceCommandsTest.java
@@ -1,24 +1,17 @@
 package org.geoserver.shell;
 
-
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
-import com.xebialabs.restito.server.StubServer;
 import org.glassfish.grizzly.http.Method;
 import org.glassfish.grizzly.http.util.HttpStatus;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.springframework.shell.support.util.OsUtils;
-
-import java.io.IOException;
 
 import static com.xebialabs.restito.builder.stub.StubHttp.whenHttp;
 import static com.xebialabs.restito.builder.verify.VerifyHttp.verifyHttp;
 import static com.xebialabs.restito.semantics.Action.status;
 import static com.xebialabs.restito.semantics.Action.stringContent;
 import static com.xebialabs.restito.semantics.Condition.*;
-import static junit.framework.Assert.*;
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
 public class WorkspaceCommandsTest extends BaseTest {
 
@@ -79,4 +72,33 @@ public class WorkspaceCommandsTest extends BaseTest {
         verifyHttp(server).once(method(Method.POST), uri("/geoserver/rest/workspaces"));
     }
 
+    @Test
+    public void getDefaultWorkspace() throws Exception {
+        whenHttp(server).match(get("/geoserver/rest/workspaces/default.xml")).then(stringContent("<workspace><name>cite</name></workspace>"), status(HttpStatus.OK_200));
+
+        Geoserver geoserver = new Geoserver("http://00.0.0.0:8888/geoserver", "admin", "geoserver");
+        WorkspaceCommands commands = new WorkspaceCommands();
+        commands.setGeoserver(geoserver);
+        String actual = commands.getDefault();
+        String expected = "cite";
+        assertEquals(expected, actual);
+
+        verifyHttp(server).once(method(Method.GET), uri("/geoserver/rest/workspaces/default.xml"));
+    }
+
+    @Test
+    public void setDefaultWorkspace() throws Exception {
+        whenHttp(server).match(put("/geoserver/rest/workspaces/default.xml")).then(stringContent("true"), status(HttpStatus.OK_200));
+
+        Geoserver geoserver = new Geoserver("http://00.0.0.0:8888/geoserver", "admin", "geoserver");
+        WorkspaceCommands commands = new WorkspaceCommands();
+        commands.setGeoserver(geoserver);
+        boolean result = commands.setDefault("topp");
+        assertTrue(result);
+        String actual = server.getCalls().get(0).getPostBody();
+        String expected = "<workspace><name>topp</name></workspace>";
+        assertEquals(expected, actual);
+
+        verifyHttp(server).once(method(Method.PUT), uri("/geoserver/rest/workspaces/default.xml"));
+    }
 }
