8441f5d02f5b04dc91223d7f234620efbc98e73d
jericks <jared.erickson@gmail.com>
2018-02-21 19:59:28 -0800

    Use maven assembly filtering to automatically add version to scripts

--- src/main/bin/gs-shell.bat
+++ src/main/bin/gs-shell.bat
@@ -29,7 +29,7 @@ set JAVA="%JAVA_HOME%\bin\java.exe"
 set CWD=%CD%
 cd %~dp0../lib
 
-%JAVA% -jar geoserver-shell-0.4-SNAPSHOT.jar %*
+%JAVA% -jar geoserver-shell-${project.version}.jar %*
 goto end
 
 :error
